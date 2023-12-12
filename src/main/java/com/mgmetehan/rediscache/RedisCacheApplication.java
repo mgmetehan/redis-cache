cbb634da883dda93949d183ca132b43fbd5a2003
mgmetehan <mgmetehan@hotmail.com>
2023-12-12 19:25:40 +0300

    init project

---
+++ src/main/java/com/mgmetehan/rediscache/RedisCacheApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.rediscache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisCacheApplication.class, args);
+	}
+
+}
